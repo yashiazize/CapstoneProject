c563db211dbc9ac9c928d671b3a974da86662e3d
Yashiry Azize <azizeyashiry@gmail.com>
2021-08-30 19:45:43 -0400

    create schema

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -1,6 +1,28 @@
-DROP TABLE IF EXISTS test;
+DROP TABLE IF EXISTS users;
 
-CREATE TABLE test (
+CREATE TABLE users (
     id SERIAL PRIMARY KEY, 
-    name TEXT
+    email VARCHAR(100) unique, 
+    pw_hash TEXT NOT NULL, 
+    name VARCHAR(100) NOT NULL, 
+    is_chef BOOLEAN NOT NULL DEFAULT FALSE, 
+    cuisine TEXT NOT NULL, 
+    zip_code VARCHAR(5) NOT NULL, 
+    image INT NOT NULL
 );
+
+CREATE TABLE bookings (
+    id SERIAL PRIMARY KEY, 
+    chef_id INT REFERENCES users (id) NOT NULL, 
+    user_id INT REFERENCES users (id) NOT NULL, 
+    status TEXT NOT NULL, 
+    cuisine TEXT NOT NULL, 
+    event_date DATE NOT NULL, 
+    start_time TIME NOT NULL, 
+    end_time TIME NOT NULL
+)
+
+CREATE TABLE ratings (
+    rating NUMERIC NOT NULL CHECK (rating >=0 AND rating <= 5), 
+    booking_id INT f_key: ref (bookings.id)
+)
