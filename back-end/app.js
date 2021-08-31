e385401396dbffdb7dc1db6cf9956f557da3d904
Marsha Hall <mphall77@gmail.com>
2021-08-30 21:48:12 -0400

    add users controller (#5)

--- back-end/app.js
+++ back-end/app.js
@@ -1,27 +1,16 @@
 const cors = require("cors");
 const express = require("express");
+const usersController = require("./controllers/usersController");
 
 const app = express();
 
-
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
+app.use("/users", usersController);
 
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+	res.send("Welcome to our Chef Finder App!");
 });
-
-const db = require("./db/dbConfig.js");
-
-app.get("/users", async (req, res) => {
-  try {
-    const allDays = await db.any("SELECT * FROM users");
-    res.json(allDays);
-  } catch (err) {
-    res.json(err);
-  }
-});
-
 
 module.exports = app;
