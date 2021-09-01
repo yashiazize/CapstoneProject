81e6e9f54342b806dc7363ca8121445d2af658e0
Marsha Hall <mphall77@gmail.com>
2021-08-31 20:07:18 -0400

    Bookings seed (#8)
    * change booking schems/seed
    
    * correct chef_id, user_id in schems/seed

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -10,14 +10,16 @@ VALUES
     ('vicCruz89@yahoo.com', 'football88', 'Victor Cruz', false, NULL, NULL);
 
 
-INSERT INTO bookings (		
+INSERT INTO bookings (
+    		chef_id,
+            user_id,
 		status,
 		cuisine,
 		event_date,
 		start_time,
 		end_time) 
 VALUES
-        ('pending', 'Italian', 'Dec 31, 2021', '20:00:00', '03:00:00' ),
-        ('accepted', 'French', 'Sept 1, 2021', '19:00:00', '21:00:00'),
-        ('rejected', 'Asian', 'March 8 , 2021', '18:00:00', '22:00:00' ),
-        ('pending', 'Soul food', 'Nov 25, 2021', '11:00:00', '17:00:00');
+        (1, 3, 'pending', 'Italian', 'Dec 31, 2021', '20:00:00', '03:00:00' ),
+        (2, 5, 'accepted', 'French', 'Sept 1, 2021', '19:00:00', '21:00:00'),
+        (4, 3, 'rejected', 'Asian', 'March 8 , 2021', '18:00:00', '22:00:00' ),
+        (4, 5, 'pending', 'Soul food', 'Nov 25, 2021', '11:00:00', '17:00:00');
