11eb2c53a82bd5f6d7aaba8b3d9b8ee0945ba6e6
Marsha Hall <mphall77@gmail.com>
2021-09-04 16:46:07 -0400

    add services section to home page

---
+++ front-end/src/Components/Services.js
@@ -0,0 +1,54 @@
+import Title from "./Title";
+import { useState } from "react";
+
+import {
+	FaGlassCheers,
+	FaCookieBite,
+	FaHamburger,
+	FaCocktail,
+	FaJava,
+} from "react-icons/fa";
+
+const Services = () => {
+	const services = [
+		{
+			icon: <FaGlassCheers />,
+			title: "special events",
+			info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo vitae architecto unde harum repellat quia.",
+		},
+		{
+			icon: <FaHamburger />,
+			title: "meal prep",
+			info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo vitae architecto unde harum repellat quia.",
+		},
+		{
+			icon: <FaCocktail />,
+			title: "parties",
+			info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo vitae architecto unde harum repellat quia.",
+		},
+		{
+			icon: <FaJava />,
+			title: "business events",
+			info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo vitae architecto unde harum repellat quia.",
+		},
+	];
+
+	return (
+		<section className="services">
+			<Title title="services" />
+			<div className="services-center">
+				{services.map((service, index) => {
+					return (
+						<article key={index} className="service">
+							<span>{service.icon}</span>
+							<h6>{service.title}</h6>
+							<p>{service.info}</p>
+						</article>
+					);
+				})}
+			</div>
+		</section>
+	);
+};
+
+export default Services;
