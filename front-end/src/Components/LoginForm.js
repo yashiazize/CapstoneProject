1ffbb6d0954de3166557ae029a51b1315e61fc8d
Yashiry Azize <azizeyashiry@gmail.com>
2021-09-05 14:44:57 -0400

    fix form, add section tag

--- front-end/src/Components/LoginForm.js
+++ front-end/src/Components/LoginForm.js
@@ -1,27 +1,36 @@
 import { Link } from "react-router-dom";
-import "../Styles/LoginForm.css"
+import "../Styles/LoginForm.css";
 
 const LoginForm = () => {
   return (
-    <div className="container">
-      <p className="heading"> Login </p>
-      <div className="box">
-        <p>Email</p>
-        <div>
-          <input type="email" name="" id="" placeholder="Enter your E-mail" />
+    <section>
+      <div className="container">
+        <p className="heading"> Login </p>
+        <div className="box">
+          <p>Email</p>
+          <div>
+            <input type="email" name="" id="" placeholder="Enter your E-mail" />
+          </div>
         </div>
-      </div>
-      <div className="box">
+        <div className="box">
           <p>Password</p>
           <div>
-          <input type="password" name="" id="" placeholder="Enter your Password" />
+            <input
+              type="password"
+              name=""
+              id=""
+              placeholder="Enter your Password"
+            />
           </div>
+        </div>
+        <button className="loginBtn">Login</button>
+        <p className="text">
+          {" "}
+          Dont have an account? <Link to="/users/new">Sign up</Link>
+        </p>
       </div>
-      <button className="loginBtn">Login</button>
-      <p className="text"> Dont have an account? <Link to="/users/new">Sign up</Link></p>
-    </div>
+    </section>
   );
 };
-
 
 export default LoginForm;
--- front-end/src/Components/SignUpForm.js
+++ front-end/src/Components/SignUpForm.js
@@ -10,14 +10,15 @@ const SignUpForm = () => {
   };
 
   return (
-    <div className="container">
-      <p className="heading"> Sign Up </p>
-      <div className="box">
-        <p>First Name</p>
-        <div>
-          <input type="firstName" name="" id="" placeholder="First Name" />
+    <section>
+      <div className="container">
+        <p className="heading"> Sign Up </p>
+        <div className="box">
+          <p>First Name</p>
+          <div>
+            <input type="firstName" name="" id="" placeholder="First Name" />
+          </div>
         </div>
-		</div>
         <div className="box">
           <p>Last Name</p>
           <div>
@@ -36,30 +37,31 @@ const SignUpForm = () => {
             <input type="zipCode" name="" id="" placeholder="Enter ZipCode" />
           </div>
         </div>
-      <div className="box">
-        <p> Create Password</p>
-        <div>
-          <input
-            type="password"
-            name=""
-            id=""
-            placeholder="Enter your Password"
-          />
+        <div className="box">
+          <p> Create Password</p>
+          <div>
+            <input
+              type="password"
+              name=""
+              id=""
+              placeholder="Enter your Password"
+            />
+          </div>
         </div>
+        <div>
+          <p>Are you a Chef?</p>
+          <input
+            onClick={handleCheckBox}
+            checked={checked}
+            type="checkbox"
+            value="Chef"
+            name="is_chef"
+          />
+          {checked ? <div> </div> : null}
+        </div>
+        <button className="loginBtn">Sign Up</button>
       </div>
-      <div>
-        <p>Are you a Chef?</p>
-        <input
-          onClick={handleCheckBox}
-          checked={checked}
-          type="checkbox"
-          value="Chef"
-          name="is_chef"
-        />
-        {checked ? <div> </div> : null}
-      </div>
-      <button className="loginBtn">Sign Up</button>
-    </div>
+    </section>
   );
 };
 
