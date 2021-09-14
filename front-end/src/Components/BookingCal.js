f69ddbfab16d3fe4e5aa6acf0b9adec423bcb1a3
Yashiry Azize <azizeyashiry@gmail.com>
2021-09-14 19:05:07 -0400

    add css and bootstrap bookchef form

--- front-end/src/Components/BookingCal.js
+++ front-end/src/Components/BookingCal.js
@@ -32,72 +32,33 @@ const BookingCal = () => {
   return (
     <div>
       <form className={classes.container} noValidate>
-        <label>Select Cuisine:</label>
-        <select>
-          <option>American</option>
-          <option>Asian</option>
-          <option>Carribean</option>
-          <option>Italian</option>
-          <option>French</option>
-          <option>Mediterranean</option>
-        </select>
-
-        <label>Party Size:</label>
-        <select>
-          <option>1-2</option>
-          <option>3-4</option>
-          <option>4-10</option>
-          <option>10-20</option>
-          <option>20+</option>
-        </select>
-
-        <label>Dietary Restrictions:</label>
-        <label for="dairy_free">Dairy Free</label>
-        <input type="checkbox" name="dairy_free" />
-
-        <label for="gluten_free"> Gluten Free</label>
-        <input type="checkbox" name="gluten_free" />
-
-        <label for="kosher">Kosher</label>
-        <input type="checkbox" name="kosher" />
-
-        <h3>Event Location</h3>
-        <label>Address</label>
-        <input required />
-        <label>Apt</label>
-        <input />
-        <label>City</label>
-        <input required />
-        <label>State</label>
-        <input required />
-
-        <h3>Select date and time for booking</h3>
-        <TextField
-          id="startDate"
-          label="Event Start Date and Time"
-          type="datetime-local"
-          onChange={handleSelectedDate}
-          value={eventDate.startDate}
-          name="startDate"
-          // defaultValue="2017-05-24T10:30"
-          className={classes.textField}
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <TextField
-          id="endDate"
-          label="Event End Date and Time"
-          type="datetime-local"
-          // defaultValue={new Date()}
-          onChange={handleSelectedDate}
-          // name="endDate"
-          value={eventDate.endDate}
-          className={classes.textField}
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
+      <h3>Select date and time for booking</h3>
+      <TextField
+        id="startDate"
+        label="Event Start Date and Time"
+        type="datetime-local"
+        onChange={handleSelectedDate}
+        value={eventDate.startDate}
+        name="startDate"
+        // defaultValue="2017-05-24T10:30"
+        className={classes.textField}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+      <TextField
+        id="endDate"
+        label="Event End Date and Time"
+        type="datetime-local"
+        // defaultValue={new Date()}
+        onChange={handleSelectedDate}
+        // name="endDate"
+        value={eventDate.endDate}
+        className={classes.textField}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
       </form>
     </div>
   );
--- front-end/src/Components/NewBookingForm.js
+++ front-end/src/Components/NewBookingForm.js
@@ -6,17 +6,78 @@ import BookingCal from "./BookingCal";
 // dates are passed in from the ChefDatails Calendar
 
 const NewBookingForm = () => {
-	return (
-		<section>
-			<form>
-				<h1>Form to book a chef here</h1>
-				<BookingCal />
-				<button className="btn-primary" type="submit">
-					Submit
-				</button>
-			</form>
-		</section>
-	);
+  return (
+    <section className="BookingFormContainer">
+      <form className="row g-3">
+        <h1> Book Chef</h1>
+        <div>
+          <label>Select Cuisine:</label>
+          <select className="form-select" aria-label="Default select example">
+            <option>American</option>
+            <option>Asian</option>
+            <option>Carribean</option>
+            <option>Italian</option>
+            <option>French</option>
+            <option>Mediterranean</option>
+          </select>
+        </div>
+        <div>
+          <label>Party Size:</label>
+          <select className="form-select" aria-label="Default select example">
+            <option>1-2</option>
+            <option>3-4</option>
+            <option>4-10</option>
+            <option>10-20</option>
+            <option>20+</option>
+          </select>
+        </div>
+        <label>Dietary Restrictions:</label>
+        <div className="form-check col-auto">
+          <label for="dairy_free">Dairy Free</label>
+          <input
+            className="form-check-input"
+            type="checkbox"
+            name="dairy_free"
+          />
+        </div>
+        <div className="form-check col-auto">
+          <label for="gluten_free"> Gluten Free</label>
+          <input
+            className="form-check-input"
+            type="checkbox"
+            name="gluten_free"
+          />
+        </div>
+        <div className="form-check col-auto">
+          <label for="kosher">Kosher</label>
+          <input className="form-check-input" type="checkbox" name="kosher" />
+        </div>
+        <h3>Event Location</h3>
+        <div className="form-floating mb-3">
+          <input type="text" class="form-control" required />
+          <label for="floatingInput">Address</label>
+        </div>
+        <div className="form-floating mb-3">
+          <input type="text" class="form-control" />
+          <label for="floatingInput">Apt</label>
+        </div>
+        <div className="form-floating mb-3 col-md-6">
+          <input type="text" class="form-control" required />
+          <label for="floatingInput">City</label>
+        </div>
+        <div className="form-floating mb-3 col-md-4">
+          <input type="text" class="form-control" required />
+          <label for="floatingInput">State</label>
+        </div>
+        <div>
+          <BookingCal />
+          <button className="btn-primary" type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default withRouter(NewBookingForm);
