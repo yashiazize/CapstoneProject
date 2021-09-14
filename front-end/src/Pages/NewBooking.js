8d3e36e0b150fa34f7f232e5f263bfe0a65713d3
Marsha Hall <mphall77@gmail.com>
2021-09-14 15:15:07 -0400

    add availability to schems and seed (#21)

--- front-end/src/Pages/NewBooking.js
+++ front-end/src/Pages/NewBooking.js
@@ -1,10 +1,23 @@
 import NewBookingForm from "../Components/NewBookingForm";
+import Hero from "../Components/Hero";
+import Banner from "../Components/Banner";
+import { Link } from "react-router-dom";
 
 const NewBooking = () => {
 	return (
-		<div>
-			<NewBookingForm />
-		</div>
+		<>
+			{/* change the className and image in the css */}
+			<Hero hero="chefsHero">
+				<Banner title="chef">
+					<Link to={"/users/chefs"} className="btn-primary">
+						Back to List
+					</Link>
+				</Banner>
+			</Hero>
+			<div>
+				<NewBookingForm />
+			</div>
+		</>
 	);
 };
 
