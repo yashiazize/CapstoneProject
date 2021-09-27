b9e8a20149e6570c3b79141449020193cca13cbc
Marsha Hall <mphall77@gmail.com>
2021-09-27 12:24:08 -0400

    add Julia Child quote to chef show page banner \q

--- front-end/src/Pages/Index.js
+++ front-end/src/Pages/Index.js
@@ -4,18 +4,21 @@ import Banner from "../Components/Banner";
 import { Link } from "react-router-dom";
 
 const Index = ({ chefs }) => {
-  return (
-    <>
-      <Hero hero="chefsHero">
-        <Banner title="our chefs"></Banner>
-      </Hero>
-      <div>
-        <ul>
-          <ChefsList chefs={chefs} />
-        </ul>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Hero hero="chefsHero">
+				<Banner
+					title="our chefs"
+					subtitle='"People who love to eat are always the best people. - Julia Childs"'
+				></Banner>
+			</Hero>
+			<div>
+				<ul>
+					<ChefsList chefs={chefs} />
+				</ul>
+			</div>
+		</>
+	);
 };
 
 export default Index;
