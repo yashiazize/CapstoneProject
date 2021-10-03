9528f2414e04282b3dc46df69a5ccd0978ea4e33
Marsha Hall <mphall77@gmail.com>
2021-10-03 17:43:54 -0400

    fix name

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import { FaAlignRight } from "react-icons/fa";
 import { signOut } from "../Services/Firebase";
 import { useAuth } from "../Providers/AuthProvider";
 import AuthModal from "./AuthModal";
+import { apiURL } from "../util/apiURL";
 
+const API = apiURL();
 const NavBar = () => {
 	const [toggleMenu, setToggleMenu] = useState(false);
 	const [modalOpen, setModalOpen] = useState(false);
@@ -12,7 +15,21 @@ const NavBar = () => {
 	const handleToggle = () => {
 		setToggleMenu((isOpen) => !isOpen);
 	};
+	const [user, setUser] = useState(false);
 
+	useEffect(() => {
+		const getUser = async () => {
+			let res = await axios.get(`${API}/users/${currentUser?.uid}`);
+
+			setUser(res.data.payload);
+		};
+		getUser();
+	}, [currentUser]);
+
+	if (!user) {
+		return null;
+	}
+	debugger;
 	return (
 		<nav className="navBar">
 			<div className="nav-center">
@@ -40,11 +57,11 @@ const NavBar = () => {
 						</Link>
 					</li>
 					<li>
-						<Link to={`/users/:id/bookings`}>Requests</Link>
+						<Link to={`/users/${currentUser?.uid}/bookings`}>Requests</Link>
 					</li>
 				</ul>
 
-				<p> Hi, {currentUser && currentUser.email} </p>
+				<p> Hi, {user.first_name}</p>
 			</div>
 			{modalOpen ? <AuthModal setModalOpen={setModalOpen} /> : null}
 		</nav>
