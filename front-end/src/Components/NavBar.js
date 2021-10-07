fd14f24b6b4c9922e884bb8dd65faebae6447c0d
yashiazize <75218370+yashiazize@users.noreply.github.com>
2021-10-07 19:46:08 -0400

    Addchefsfixform (#49)
    * fixed booking form & added real chefs to back end
    
    * fix flex grid
    
    * fix navbar & login/signup button
    
    * add css to name

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -58,7 +58,6 @@ const NavBar = () => {
 								Login/Sign Up
 							</button>
 						</Link> */}
-
             <Link to={"/"} onClick={signOut}>
               <button className="lgnBtn"> Logout</button>
             </Link>
